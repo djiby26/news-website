ea877aa2134a59c984eff4a000bd692250ab1459
djiby26 <djibyndione26@gmail.com>
2021-10-19 00:02:13 +0000

    pagination request fixe

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -6,6 +6,7 @@ const Article = ({ article }) => {
       <h3>{article.title}</h3>
       <img src={article.image} alt="" />
       <p>{article.textContent}</p>
+      <p>article.image</p>
     </div>
   );
 };
